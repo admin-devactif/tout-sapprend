99180caf6d9733e279e84beb185cb77e5d485536
XDesroches <desrochesxavier@gmail.com>
2026-03-26 10:25:42 -0400

    Ajout de Font Awesome, DaisyUI et i18n

--- resources/ts/Pages/Home.vue
+++ resources/ts/Pages/Home.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { Head } from "@inertiajs/vue3";
 import Layout from "./Layout.vue";
+import { useI18n } from "vue-i18n";
 
-defineProps<{ message: string }>();
+const { t } = useI18n();
 </script>
 
 <template>
     <Layout>
         <Head title="Welcome" />
-        <h1 class="text-red-500">{{ message }}</h1>
+        <h1 class="text-red-500">{{ t("welcome") }}</h1>
     </Layout>
 </template>
--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,14 +1,28 @@
 import { createInertiaApp } from "@inertiajs/vue3";
 import { createApp, h } from "vue";
+import { createI18n } from "vue-i18n";
+
+import fr from "../js/lang/fr.json";
+import en from "../js/lang/en.json";
+
+const i18n = createI18n({
+    locale: "fr",
+    fallbackLocale: "en",
+    messages: {
+        fr,
+        en
+    }
+});
 
 void createInertiaApp({
-    resolve: name => {
+    resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
         return pages[`./Pages/${name}.vue`];
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(i18n)
             .mount(el);
     }
 });
