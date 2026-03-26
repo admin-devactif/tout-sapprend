cf2aa0d1e65f251d73f9552d89196705f148c7c5
XDesroches <desrochesxavier@gmail.com>
2026-03-26 14:04:08 -0400

    Ajout du css et decoration

--- resources/ts/Components/AppNavbar.vue
+++ resources/ts/Components/AppNavbar.vue
@@ -2,99 +2,130 @@
 import { faBars, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { Link } from "@inertiajs/vue3";
-import { ref } from "vue";
+import { useI18n } from "vue-i18n";
 
+import headerBackground from "../../assets/header/header-background.jpg";
 import logo from "../../assets/shared/LOGO_toutsapprend.png";
 
 type NavigationLink = {
     href: string;
-    label: string;
+    labelKey: string;
 };
 
+const { t } = useI18n({ useScope: "global" });
+
 const primaryLinks: NavigationLink[] = [
-    { href: "/", label: "ACCUEIL" },
-    { href: "/orthopedagogie", label: "ORTHOPÉDAGOGIE" },
-    { href: "/intervention", label: "INTERVENTION ÉDUCATIVE" },
-    { href: "/stimulation", label: "STIMULATION DU LANGAGE" },
-    { href: "/cpe", label: "CPE ET MILIEUX DE GARDE" }
+    { href: "/", labelKey: "navigation.home" },
+    { href: "/orthopedagogie", labelKey: "navigation.orthopedagogie" },
+    { href: "/intervention", labelKey: "navigation.intervention" },
+    { href: "/stimulation", labelKey: "navigation.stimulation" },
+    { href: "/cpe", labelKey: "navigation.cpe" }
 ];
 
 const secondaryLinks: NavigationLink[] = [
-    { href: "/contact", label: "NOUS JOINDRE" },
-    { href: "/appointment", label: "PRENDRE RENDEZ-VOUS" }
+    { href: "/contact", labelKey: "navigation.contact" },
+    { href: "/appointment", labelKey: "navigation.appointment" }
 ];
-
-const isMobileMenuOpen = ref(false);
 </script>
 
 <template>
-    <div class="navbar bg-base-100 shadow">
-        <div class="container mx-auto block">
-            <div class="navbar justify-between lg:hidden">
-                <Link href="/" class="btn btn-ghost normal-case">
-                    <img :src="logo" alt="Tout s'apprend" class="h-12 w-12 rounded-full" />
-                    <span>Tout s'apprend</span>
-                </Link>
+    <div class="border-b border-orange-100 bg-white">
+        <div class="relative overflow-visible">
+            <div
+                class="absolute inset-0 bg-cover bg-center opacity-80"
+                :style="{ backgroundImage: `url(${headerBackground})` }"></div>
+            <div class="absolute inset-0 bg-white/45"></div>
 
-                <button
-                    type="button"
-                    class="btn btn-circle btn-ghost"
-                    aria-label="Ouvrir le menu"
-                    @click="isMobileMenuOpen = !isMobileMenuOpen">
-                    <FontAwesomeIcon :icon="faBars" />
-                </button>
-            </div>
-
-            <div class="navbar hidden lg:flex">
-                <div class="navbar-start">
-                    <Link href="/" class="shrink-0">
-                        <img :src="logo" alt="Tout s'apprend" class="h-16 w-16 rounded-full" />
+            <div class="relative mx-auto max-w-7xl px-2 py-1 sm:px-4 lg:px-6">
+                <div class="navbar justify-between px-0 lg:hidden">
+                    <Link
+                        href="/"
+                        class="btn btn-ghost border-0 h-auto gap-3 px-0 normal-case bg-transparent shadow-none">
+                        <img
+                            :src="logo"
+                            alt="Tout s'apprend"
+                            class="size-20 rounded-full p-1" />
                     </Link>
-                </div>
 
-                <div class="navbar-center flex-1">
-                    <div class="w-full">
-                        <ul class="menu menu-horizontal w-full justify-center gap-2">
-                            <li v-for="link in primaryLinks" :key="link.label">
-                                <Link :href="link.href" class="btn btn-outline">
-                                    {{ link.label }}
-                                </Link>
-                            </li>
-                        </ul>
+                    <div class="dropdown dropdown-end">
+                        <div
+                            tabindex="0"
+                            role="button"
+                            class="btn btn-circle btn-outline border-orange-300 bg-white/80 text-orange-400 shadow-sm hover:bg-orange-50">
+                            <FontAwesomeIcon :icon="faBars" class="text-lg" />
+                        </div>
 
-                        <ul class="menu menu-horizontal mt-2 w-full justify-center gap-2">
-                            <li v-for="link in secondaryLinks" :key="link.label">
-                                <Link :href="link.href" class="btn btn-outline">
-                                    {{ link.label }}
-                                </Link>
-                            </li>
-                        </ul>
+                        <div
+                            tabindex="0"
+                            class="dropdown-content right-0 z-50 mt-3 w-80 max-w-[calc(100vw-1rem)] rounded-3xl border border-orange-100 bg-white/95 p-4 shadow-lg backdrop-blur-sm">
+                            <ul class="menu w-full items-center gap-3 rounded-box bg-transparent p-0">
+                                <li
+                                    v-for="link in [...primaryLinks, ...secondaryLinks]"
+                                    :key="`mobile-${link.labelKey}`"
+                                    class="flex w-full justify-center">
+                                    <Link
+                                        :href="link.href"
+                                        class="btn btn-outline min-h-12 w-full justify-center rounded-full border-orange-300 bg-white px-4 py-3 text-center text-sm font-light tracking-wide text-orange-400 shadow-none hover:bg-orange-50">
+                                        {{ t(link.labelKey) }}
+                                    </Link>
+                                </li>
+                            </ul>
+
+                            <div class="mt-4 flex flex-col items-center gap-2 text-sm text-stone-500">
+                                <a
+                                    href="mailto:info@serviceseducatifs.ca"
+                                    class="link link-hover inline-flex items-center gap-2 no-underline hover:text-orange-400">
+                                    <FontAwesomeIcon :icon="faEnvelope" />
+                                    <span>info@serviceseducatifs.ca</span>
+                                </a>
+                                <a
+                                    href="tel:4508814432"
+                                    class="link link-hover inline-flex items-center gap-2 no-underline hover:text-orange-400">
+                                    <FontAwesomeIcon :icon="faPhone" />
+                                    <span>450-881-4432</span>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div v-if="isMobileMenuOpen" class="pb-4 lg:hidden">
-                <div class="card bg-base-100 shadow">
-                    <div class="card-body">
-                        <ul class="menu gap-2">
-                            <li
-                                v-for="link in [...primaryLinks, ...secondaryLinks]"
-                                :key="`mobile-${link.label}`">
-                                <Link :href="link.href" class="btn btn-outline" @click="isMobileMenuOpen = false">
-                                    {{ link.label }}
+                <div class="hidden items-center gap-6 py-1 lg:flex">
+                    <Link href="/" class="flex-none">
+                        <img
+                            :src="logo"
+                            alt="Tout s'apprend"
+                            class="size-28 rounded-full p-1" />
+                    </Link>
+
+                    <div class="flex-1">
+                        <div class="menu menu-horizontal w-full gap-3 bg-transparent p-0">
+                            <div
+                                v-for="link in primaryLinks"
+                                :key="link.labelKey"
+                                class="flex flex-1">
+                                <Link
+                                    :href="link.href"
+                                    class="btn btn-outline min-h-12 w-full rounded-full border-orange-300 bg-white/70 px-4 py-3 text-center text-xs font-light tracking-widest text-orange-400 shadow-none hover:bg-orange-50">
+                                    <span class="leading-tight whitespace-normal">
+                                        {{ t(link.labelKey) }}
+                                    </span>
                                 </Link>
-                            </li>
-                        </ul>
+                            </div>
+                        </div>
 
-                        <div class="flex flex-col gap-2">
-                            <a href="mailto:info@serviceseducatifs.ca" class="link link-hover inline-flex items-center gap-2">
-                                <FontAwesomeIcon :icon="faEnvelope" />
-                                <span>info@serviceseducatifs.ca</span>
-                            </a>
-                            <a href="tel:4508814432" class="link link-hover inline-flex items-center gap-2">
-                                <FontAwesomeIcon :icon="faPhone" />
-                                <span>450-881-4432</span>
-                            </a>
+                        <div class="menu menu-horizontal mt-3 w-full gap-3 bg-transparent p-0">
+                            <div
+                                v-for="link in secondaryLinks"
+                                :key="link.labelKey"
+                                class="flex flex-1">
+                                <Link
+                                    :href="link.href"
+                                    class="btn btn-outline min-h-12 w-full rounded-full border-orange-300 bg-white/70 px-4 py-3 text-center text-xs font-light tracking-widest text-orange-400 shadow-none hover:bg-orange-50">
+                                    <span class="leading-tight whitespace-normal">
+                                        {{ t(link.labelKey) }}
+                                    </span>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
